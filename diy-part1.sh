4c2d6442ac7502515ca768836402e19aed0a67e5
Neting <532501838@qq.com>
2020-12-13 19:58:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add Passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall # passwall依赖
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-package
